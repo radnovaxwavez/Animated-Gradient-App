ba412a1e9c21e1de4ce73b5b0169c1fdbffb07d7
Oscar <Oscar@LAPTOP-VSUJG3AJ>
2022-06-01 22:15:27 +0100

    Add project files.

---
+++ AnimatedGradient/AnimatedGradient/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnimatedGradient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Device.SetFlags(new[] { "Brush_Experimantal" }); //This is here because the Brush is still an experimantal component
+            MainPage = new MainPage();                       //It's kind of just a disclaimer to the program
+        } 
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
